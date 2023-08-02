c9177c2efd8b00238341eb276701de70667a5181
MahesaSyawalA <mahesaabd7@gmail.com>
2023-08-02 09:19:16 +0700

    hidden component to mobile device

--- src/components/content/heroComponent.js
+++ src/components/content/heroComponent.js
@@ -2,11 +2,11 @@ import React from "react";
 
 export function HeroComponent({Gambar1}) {
   return (
-    <div className=" flex justify-center items-center w-full h-[416px] lg:h-[760px] lg:pt-20">
+    <div className=" border flex justify-center items-center w-full h-[416px] lg:h-[760px] lg:pt-20">
       <div className="absolute w-[50%] h-[100%] bg-gradient-to-br from-[#8AAAE5] bg-opacity-0 blur-3xl rounded-full -top-[20%] -left-[40%] "></div>
-      <div className="absolute -z-10 w-[80%] h-[100%] bg-gradient-to-br from-[#8AAAE5] bg-opacity-0 blur-3xl rounded-full -bottom-[50%] -right-[75%] "></div>
+      <div className="hidden absolute -z-30 w-[80%] h-[60%] lg:h-[100%] bg-gradient-to-br from-[#8AAAE5] bg-opacity-0 blur-3xl rounded-full -bottom-[50%] -right-[75%] "></div>
       <div className="flex flex-col lg:flex-row-reverse items-center lg:items-start justify-center h-[80%] w-[90%] gap-2 lg:gap-10 pt-5 lg:pt-0 ">
-        <div className=" bg-gradient-to-br from-[#8AAAE5] bg-opacity-0 w-[50%] h-[85%]  lg:w-[35%] lg:h-[78%] rounded-full ">
+        <div className=" bg-gradient-to-br from-[#8AAAE5] bg-opacity-0 w-[45%] h-[85%]  lg:w-[35%] lg:h-[78%] rounded-full ">
           <div className=" w-[100%] h-[100%] bg-cover bg-center rounded-full "
         style={{
           backgroundImage: `url(${Gambar1})`
