8a8fc6616d6bb036fa14cda65911601afa0e0511
MahesaSyawalA <mahesaabd7@gmail.com>
2023-08-06 14:46:09 +0700

    fix responsive for mobile

--- src/components/content/otherComponent.js
+++ src/components/content/otherComponent.js
@@ -5,20 +5,20 @@ import {GiWaterfall} from "react-icons/gi";
 import {DiScrum} from "react-icons/di";
 export function OtherComponent() {
   return (
-    <div className=" pb-36 flex items-center flex-col w-full gap-8 ">
-      <div className="flex flex-grow items-center justify-between ">
-        <h1 className="text-3xl text-left w-[20%] font-extrabold">
+    <div className="px-8 lg:px-0 pb-36 flex items-center flex-col w-full gap-8 ">
+      <div className="flex flex-grow items-center justify-between gap-3 lg:gap-0">
+        <h1 className="text-xl lg:text-3xl text-left lg:w-[20%] font-extrabold">
           {" "}
           Other
           <br /> My Skills
         </h1>
-        <p className="text-left text-md font-bold text-[#8AAAE5]">
+        <p className="text-left text-sm lg:text-md font-bold text-[#8AAAE5]">
           Another useful skill in the field of web development, which I learned
           during fieldwork and internships
         </p>
       </div>
-      <div className="flex w-[60%] justify-between py-6 ">
-        <div className="flex flex-col gap- w-[30%] border border-[#8AAAE5] p-5 rounded-md">
+      <div className="flex flex-col lg:flex-row gap-4 lg:gap-0 w-full lg:w-[60%] justify-between py-2 lg:py-6 ">
+        <div className="flex flex-col w-full lg:w-[30%] border border-[#8AAAE5] p-5 rounded-md">
           <div className="flex justify-center items-center gap-10 p-5 text-[#8AAAE5]">
             <FaGithub size={40} />
             <FaGitlab size={40} />
@@ -30,7 +30,7 @@ export function OtherComponent() {
             </p>
           </div>
         </div>
-        <div className="flex flex-col gap- w-[30%] border border-[#8AAAE5] p-5 rounded-md">
+        <div className="flex flex-col w-full lg:w-[30%] border border-[#8AAAE5] p-5 rounded-md">
           <div className="flex justify-center items-center gap-10 p-5 text-[#8AAAE5]">
             <GiWaterfall size={40} />
             <DiScrum size={40} />
@@ -45,7 +45,7 @@ export function OtherComponent() {
             </p>
           </div>
         </div>
-        <div className="flex flex-col  w-[30%] border border-[#8AAAE5] p-5 rounded-md">
+        <div className="flex flex-col  w-full lg:w-[30%] border border-[#8AAAE5] p-5 rounded-md">
           <div className="flex justify-center items-center gap-10 p-5 text-[#8AAAE5]">
             <FaNpm size={40} />
             <GoPackageDependencies size={40} />
--- src/components/footerComponent.js
+++ src/components/footerComponent.js
@@ -5,14 +5,14 @@ export function FooterComponent() {
   return (
     <div className="flex flex-col pt-10 px-10 gap-8 bg-[#8AAAE5] ">
       <div className="flex justify-between items-center text-white text-left">
-        <h1 className="text-4xl font-bold">
+        <h1 className="text-md lg:text-4xl font-bold">
           Die As A Legend
           <br />
           Or
           <br />
           Die As A Trash
         </h1>
-        <div className="flex ">
+        <div className="flex text-[10px] lg:text-base ">
           <ul className="">
             <li className="font-semibold">Contact & My Social Media</li>
             <li className="flex gap-2 items-center">
@@ -27,9 +27,9 @@ export function FooterComponent() {
           </ul>
         </div>
       </div>
-      <div className="flex items-center justify-between py-5 border-t-2 border-white text-sm text-white">
+      <div className="flex items-center justify-between py-5 border-t-2 border-white text-xs lg:text-sm text-white">
         <p>&copy;2023 Mahesa Syawal Abdurahman</p>
-        <div className="flex gap-3">
+        <div className="flex gap-1 lg:gap-3">
           <FaInstagram size={20} color="white" />
           <FaGithub size={20} color="white" />
           <FaGitlab size={20} color="white" />
